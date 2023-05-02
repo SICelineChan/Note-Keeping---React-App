0be148648e7acd9e4391714a0c5bedb9b1f5491b
SICelineChan <108868474+SICelineChan@users.noreply.github.com>
2023-05-02 16:05:00 +0200

    completed stage 1

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './style.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Notes from './components/Notes';
 
 export default function App() {
   return (
     <div>
       <Header />
+      <Notes />
       <p>Start editing to see some magic happen :)</p>
       <Footer />
     </div>
---
+++ src/components/Notes.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../style.css';
+
+export default function Notes() {
+  return (
+    <div className="note">
+      <h1>Title for the note</h1>
+      <p>Some content for the note</p>
+    </div>
+  );
+}
